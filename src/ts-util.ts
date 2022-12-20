5b6f25004c8c9fdfa0a3f022ec1930926a86b755
Elian Cordoba <eliancordoba98@gmail.com>
2022-12-20 20:21:17 -0300

    WIP

--- src/ts-util.ts
+++ src/ts-util.ts
@@ -21,30 +21,20 @@ export function getNodesArray(source: string) {
   );
 
   const nodes: Node[] = [];
+  const allNodes: Node[] = []
 
-  function walk(node: TSNode, expressionNumber = 0) {
-    const hasText = node.text;
+  function walk(node: TSNode, expressionNumber = 0, depth = 0) {
+    const k = formatSyntaxKind(node.kind, node.text)
+    const n = new Node({ start: 0, end: 0, kind: node.kind, text: 'no-text', lineNumber: -1, expressionNumber: depth })
+
+    allNodes.push(n)
+    const hasText = typeof node.text !== 'undefined' ? node.getText() : undefined;
     const isReservedWord = node.kind >= SyntaxKind.FirstKeyword && node.kind <= SyntaxKind.LastKeyword;
     const isPunctuation = node.kind >= SyntaxKind.FirstPunctuation && node.kind <= SyntaxKind.LastPunctuation;
 
-    const isElement = node.kind >= SyntaxKind.Block && node.kind <= SyntaxKind.MissingDeclaration;
-
-    const isExpression = node.kind >= SyntaxKind.ArrayLiteralExpression && node.kind <= SyntaxKind.SatisfiesExpression || isElement; // || node.kind === SyntaxKind.SyntaxList
-
-    if (isExpression) {
-      expressionNumber++;
-    } else {
-      expressionNumber += 0.1;
-    }
-
     // Only include visible node, nodes that represent some text in the source code.
     if (hasText || isReservedWord || isPunctuation) {
-      const prettyKind = formatSyntaxKind(node);
       const lineNumber = getLineNumber(sourceFile, node.pos + node.getLeadingTriviaWidth());
-      // TODO: Remove round for debugging
-      const _expressionNumber = Math.round(expressionNumber);
-
-      /// TODO: Store expression start and end?
 
       // Each node owns the trivia before until the previous token, for example:
       //
@@ -56,11 +46,11 @@ export function getNodesArray(source: string) {
       // value of the node starts and not where the trivia starts
       const start = node.pos + node.getLeadingTriviaWidth();
 
-      // TODO: The 0 is temporal, read more bellow
-      nodes.push(new Node(0, start, node.end, node.kind, node.text, prettyKind, _expressionNumber, lineNumber));
+      nodes.push(new Node({ start, end: node.end, kind: node.kind, text: hasText!, expressionNumber: depth, lineNumber }));
     }
 
-    node.getChildren().forEach((x) => walk(x as TSNode, expressionNumber));
+    depth++;
+    node.getChildren().forEach((x) => walk(x as TSNode, expressionNumber, depth));
   }
 
   sourceFile.getChildren().forEach((x) => walk(x as TSNode));
@@ -70,7 +60,9 @@ export function getNodesArray(source: string) {
     node.index = i;
   });
 
-  return nodes;
+  // console.table(nodes.map(x => ({ kind: x.prettyKind, depth: x.depth })))
+
+  return [nodes, allNodes];
 }
 
 // This wrapper exists because the underling TS function is marked as internal
