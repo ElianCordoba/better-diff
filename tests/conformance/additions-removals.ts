1e1c2010d421f01fb7fcbc96b108ff1f79d77e39
Elian Cordoba <eliancordoba98@gmail.com>
2023-02-03 00:12:56 -0300

    Migrated test file

---
+++ tests/conformance/additions-removals.ts
@@ -0,0 +1,110 @@
+import { describe } from "vitest";
+import { test } from "../utils";
+
+describe("Properly report lines added and removed", () => {
+  test({
+    name: "Single line added bellow",
+    a: `
+      let name;
+    `,
+    b: `
+      let name;
+      let age;
+    `,
+    expB: `
+      let name;
+      ➕let➕ ➕age;➕
+    `
+  })
+
+  test({
+    name: "Single line added above",
+    a: `
+      let name;
+    `,
+    b: `
+      let age;
+      let name;
+    `,
+    expA: `
+      1🔀let name;⏹️
+    `,
+    expB: `
+      ➕let➕ ➕age;➕
+      1🔀let name;⏹️
+    `
+  })
+
+  test({
+    name: "Multiple lines added 1",
+    b: `
+      let a;
+      let b;
+    `,
+    expB: `
+      ➕let➕ ➕a;➕
+      ➕let➕ ➕b;➕
+    `
+  })
+
+  test({
+    name: "Multiple lines added 2",
+    b: `
+      let a;
+      let b;
+      let c;
+    `,
+    expB: `
+      ➕let➕ ➕a;➕
+      ➕let➕ ➕b;➕
+      ➕let➕ ➕c;➕
+    `
+  })
+
+  test({
+    name: "Multiple lines added 3. With trivia",
+    b: `let a;
+      let b;
+    `,
+    expB: `➕let➕ ➕a;➕
+    ➕let➕ ➕b;➕
+  `
+  })
+
+  test({
+    name: "Added wrapped code",
+    a: `
+      callFn()
+    `,
+    b: `
+      while (true) {
+        callFn()
+      }
+    `,
+    expA: `
+      1🔀callFn()⏹️
+    `,
+    expB: `
+      ➕while➕ ➕(true)➕ ➕{➕
+        1🔀callFn()⏹️
+      ➕}➕
+    `
+  })
+
+  // This used to crash
+  test({
+    name: "Mix",
+    a: `
+      3
+    `,
+    b: `
+      () {}
+    `,
+    expA: `
+      ➖3➖
+    `,
+    expB: `
+      ➕()➕ ➕{}➕
+    `
+  })
+});
