fcc947008ac201997a3fe1d01a0da510cd95b440
Elian Cordoba <eliancordoba98@gmail.com>
2023-07-14 14:09:18 -0300

    Move utils to a different file

--- src/frontend/typescript.ts
+++ src/frontend/typescript.ts
@@ -1,8 +1,9 @@
-import ts, { SourceFile } from "typescript";
+import ts from "typescript";
 import { Node } from "../node";
 import { k } from "../reporter";
 import { _context, getOptions } from "..";
 import { KindTable, Side } from "../types";
+import { getIfNodeCanBeMatchedAlone, getLineMap, getLineNumber, getSourceFile } from "./utils";
 
 type TSNode = ts.Node & { text: string };
 
@@ -99,8 +100,6 @@ export function getNodes(side: Side, source: string): { nodes: Node[]; kindTable
       kindTable.set(node.kind, new Set([i]));
     }
 
-    //
-
     const line = node.lineNumberStart;
 
     if (nodesPerLine.has(line)) {
@@ -108,8 +107,6 @@ export function getNodes(side: Side, source: string): { nodes: Node[]; kindTable
     } else {
       nodesPerLine.set(line, new Set([node.index]));
     }
-
-    //
 
     i++;
   }
@@ -137,40 +134,4 @@ export function getNodes(side: Side, source: string): { nodes: Node[]; kindTable
     nodes,
     kindTable,
   };
-}
-
-function getSourceFile(source: string): SourceFile {
-  return ts.createSourceFile(
-    "source.ts",
-    source,
-    ts.ScriptTarget.ESNext,
-    true,
-  );
-}
-
-// All the bellow defined functions are wrappers of TS functions. This is because the underling TS is marked as internal thus there is no type information available
-
-// An array of the positions (of characters) at which the lines in the source code start, for example:
-// [0, 1, 5, 10] means that the first line start at 0 and ends at 1 (non inclusive), next one start at 1 and ends at 5 and so on.
-export function getLineMap(source: string): number[] {
-  const sourceFile = getSourceFile(source);
-  return (ts as any).getLineStarts(sourceFile);
-}
-
-// Get the line number (1-indexed) of a given character
-function getLineNumber(sourceFile: ts.SourceFile, pos: number) {
-  return (ts as any).getLineAndCharacterOfPosition(sourceFile, pos).line + 1;
-}
-
-function getIfNodeCanBeMatchedAlone(kind: number) {
-  const isLiteral = kind >= ts.SyntaxKind.FirstLiteralToken && kind <= ts.SyntaxKind.LastLiteralToken;
-  const isIdentifier = kind === ts.SyntaxKind.Identifier || kind === ts.SyntaxKind.PrivateIdentifier;
-  const isTemplate = kind === ts.SyntaxKind.FirstTemplateToken || kind === ts.SyntaxKind.LastTemplateToken;
-  const other = kind === ts.SyntaxKind.DebuggerKeyword;
-
-  if (isLiteral || isIdentifier || isTemplate || other) {
-    return true;
-  } else {
-    return false;
-  }
 }
